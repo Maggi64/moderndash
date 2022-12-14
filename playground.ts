b10a91b049f52d929fef4792e33aa998124adfcb
Maximilian Dewald <maximilian@dewaldnet.de>
2022-12-14 16:30:59 +0100

    playground

---
+++ playground.ts
@@ -0,0 +1,3 @@
+import { union } from '@array/union';
+
+console.log(union([1, 2, 3], [2, 3, 4], [3, 4]));
--- playground/playground.ts
+++
@@ -1,8 +0,0 @@
-import { union } from '@array/union';
-
-const array1 = [1, 2, 3];
-const array2 = [2, 3, 4];
-const array3 = [3, 4];
-
-const result = union(array1, array2, array3);
-console.log(result);
