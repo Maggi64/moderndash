d7fc70d9c3de397d979f3fb1fcdc76de6ced035d
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-10 14:40:13 +0100

    simplified vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,8 @@ import { fileURLToPath, URL } from 'url';
 
 import { defineConfig } from 'vite';
 
+const getPath = (path: string) => fileURLToPath(new URL(path, import.meta.url));
+
 export default defineConfig({
     test: {
         /* for example, use global to avoid globals imports (describe, test, expect): */
@@ -12,14 +14,14 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@array': fileURLToPath(new URL('src/array', import.meta.url)),
-            '@lang': fileURLToPath(new URL('src/lang', import.meta.url)),
-            '@function': fileURLToPath(new URL('src/function', import.meta.url)),
-            '@collection': fileURLToPath(new URL('src/collection', import.meta.url)),
-            '@object': fileURLToPath(new URL('src/object', import.meta.url)),
-            '@string': fileURLToPath(new URL('src/string', import.meta.url)),
-            '@number': fileURLToPath(new URL('src/number', import.meta.url)),
-            '@helpers': fileURLToPath(new URL('src/helpers', import.meta.url))
+            '@array': getPath('src/array'),
+            '@lang': getPath('src/lang'),
+            '@object': getPath('src/object'),
+            '@function': getPath('src/function'),
+            '@collection': getPath('src/collection'),
+            '@string': getPath('src/string'),
+            '@number': getPath('src/number'),
+            '@helpers': getPath('src/helpers')
         }
     }
 });
