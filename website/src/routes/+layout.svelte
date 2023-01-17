56ab37fd1c7d16ace4cf782df447a991679daa20
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-17 17:12:38 +0100

    cleanup layout

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
     import '../app.css';
 
-    import extractedTypes from '$assets/extractedTypes.json';
     import Header from '$components/Header.svelte';
     import Sidebar from '$components/sidebar/Sidebar.svelte';
-    import MobileSidebar from '$components/sidebar/SideBarBtn.svelte';
+    import SidebarBtn from '$components/sidebar/SideBarBtn.svelte';
 </script>
 
 <Header/>
 <main class="relative flex justify-center mx-auto max-w-8xl sm:px-2 lg:px-8 xl:px-12">
-    <MobileSidebar/>
+    <SidebarBtn/>
     <Sidebar/>
     <div class="flex-auto max-w-2xl min-w-0 px-4 py-10 lg:max-w-none lg:pr-0 lg:pl-8 xl:px-16">
         <slot/>
