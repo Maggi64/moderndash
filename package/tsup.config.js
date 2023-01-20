f758af20848d2ae1fd3f14646685fd450ef2bb14
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-21 00:17:23 +0100

    try to fix cjs build

--- package/tsup.config.js
+++ package/tsup.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['esm', "cjs"],
+    format: ['esm', 'cjs'],
     sourcemap: true,
     clean: true,
     outDir: 'dist',
