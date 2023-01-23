8151652fd56bfac2e9de7d85bdfbad2df308c10c
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-23 14:09:54 +0100

    add sleep function

---
+++ package/src/promise/sleep.ts
@@ -0,0 +1,12 @@
+/**
+ * Sleeps for the given amount of time.
+ *
+ * @example
+ * await sleep(1000);
+ * // => Waits for 1 second.
+ * @param ms -  Amount of time to sleep in milliseconds.
+ * @returns A promise that resolves after the given amount of time.
+ */
+export function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
