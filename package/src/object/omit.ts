c8f8ab379fe4621d06f5e12e257ace1683d1ad0b
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-24 19:05:38 +0100

    add omit documentation

--- package/src/object/omit.ts
+++ package/src/object/omit.ts
@@ -1,5 +1,19 @@
 import { pick } from './pick.js';
 
+/**
+ * Omit specified keys from an object
+ *
+ * @example
+ * const obj = {a: 1, b: 2, c: 3};
+ * omit(obj, ['a', 'b']);
+ * // => {c: 3}
+ *
+ * @param object - The object to filter
+ * @param keysToOmit - The keys to exclude from the returned object
+ * @returns - An object without the specified keys
+ *
+ */
+
 export function omit<TObj extends object, Key extends keyof TObj>(object: TObj, keysToOmit: Key[]): Omit<TObj, Key> {
     const keys = Object.keys(object);
     const filteredKeys = keys.filter(key => !keysToOmit.includes(key as Key)) as Exclude<keyof TObj, Key>[];
