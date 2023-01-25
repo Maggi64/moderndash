6d448f12390e4fa7305f179cfb3a502309cc5908
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-25 15:56:43 +0100

    feat: Consolidate unique functions into one

---
+++ package/src/array/unique.ts
@@ -0,0 +1,37 @@
+import type { IterateeFunction } from 'src/types.js';
+
+import { isEqual } from '@validate/isEqual';
+
+/**
+ * Creates a duplicate-free version of an array, in which only the first occurrence of each element is kept.
+ * The order of result values is determined by the order they occur in the array.
+ *
+ * An iteratee function is optional, to specify the value to be compared.
+ *
+ * @example
+ * unique([2, 1, 2])
+ * // => [2, 1]
+ *
+ * const users = [
+ *      { id: 1, name: 'a' },
+ *      { id: 1, name: 'c' }
+ * ]
+ *
+ * unique(users, value => value.id)
+ * // => [{ id: 1, name: 'a' }]
+ *
+ *
+ * @category Array
+ * @param array - The array to inspect.
+ * @param iteratee - The iteratee invoked per element.
+ * @returns Returns the new duplicate free array.
+ */
+
+export function unique<TInput>(array: TInput[], iteratee?: IterateeFunction<TInput>): TInput[] {
+    const compareFn = (a: TInput, b: TInput) =>
+        isEqual(iteratee ? iteratee(a) : a, iteratee ? iteratee(b) : b);
+
+    return array.filter((value, index, self) => {
+        return self.findIndex(otherValue => compareFn(value, otherValue)) === index;
+    });
+}
--- package/test/array/unique.test.ts
+++ package/test/array/unique.test.ts
@@ -1,18 +1,18 @@
 import { test, expect, describe } from 'vitest';
 
-import { uniq } from '@array/uniq';
+import { unique } from '@array/unique';
 
 describe('uniq', () => {
     test('should return an array with only unique values', () => {
         const input = [1, 2, 3, 3, 4, 5, 5, 6];
         const expectedOutput = [1, 2, 3, 4, 5, 6];
-        expect(uniq(input)).toEqual(expectedOutput);
+        expect(unique(input)).toEqual(expectedOutput);
     });
 
     test('should handle objects and arrays correctly', () => {
         const input = [{ a: 1 }, { a: 2 }, { a: 1 }, [1, 2], [1, 2], [1, 2, 3]
         ];
         const expectedOutput = [{ a: 1 }, { a: 2 }, [1, 2], [1, 2, 3]];
-        expect(uniq(input)).toEqual(expectedOutput);
+        expect(unique(input)).toEqual(expectedOutput);
     });
 });
