b2c5efe956796dad86c36c71eb768b144add1b24
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-28 13:08:22 +0100

    feat: consolidate sample into 1 func

--- package/src/array/sample.ts
+++ package/src/array/sample.ts
@@ -1,18 +1,36 @@
 /**
- * Gets a random element from `array`.
+ * Gets a random element an array. A single element is returned by default.  
+ * Specify the `size` parameter to get an array of multiple random elements.
  *
+ * If the array is empty, `undefined` is returned.
  * @example
  * sample([1, 2, 3, 4])
  * // => 2
+ *
+ * sample([1, 2, 3, 4], 2)
+ * // => [3, 1]
  * @category Array
  * @param array - The array to sample.
  * @returns Returns the random element.
  */
 
-export function sample<TInput>(array: TInput[]): TInput | undefined {
-    if (array.length === 0) {
+export function sample<TArr>(array: TArr[]): TArr | undefined;
+export function sample<TArr>(array: TArr[], size: number): TArr[];
+export function sample<TArr>(array: TArr[], size?: number): TArr | undefined | TArr[] {
+    if (array.length === 0)
         return undefined;
+    if (size === undefined)
+        return getSingleSample(array);
+
+    // Multiple samples
+    const result = new Array<TArr>(size);
+    for (let i = 0; i < size; i++) {
+        result[i] = getSingleSample(array);
     }
+    return result;
+}
+
+function getSingleSample<TArr>(array: TArr[]): TArr {
     const randomIndex = Math.floor(Math.random() * array.length);
     return array[randomIndex];
 }
--- package/test/array/sample.test.ts
+++ package/test/array/sample.test.ts
@@ -14,4 +14,15 @@ describe('sample', () => {
         const result = sample(collection);
         expect(result).toBeUndefined();
     });
+
+    test('should return an empty array if the input array is empty', () => {
+        const result = sample([], 3);
+        expect(result).toBeUndefined();
+    });
+
+    test('should return a random subset of the input array', () => {
+        const array = [1, 2, 3, 4, 5];
+        const result = sample([1, 2, 3, 4, 5], 3);
+        expect(expect.arrayContaining(result)).toEqual(array);
+    });
 });
