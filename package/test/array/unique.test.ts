9d84ce2a687340b26d49005dfba2526715aafa4c
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-28 10:53:51 +0100

    feat: remove zip & unzip due to low use

--- package/test/array/unique.test.ts
+++ package/test/array/unique.test.ts
@@ -2,7 +2,7 @@ import { test, expect, describe } from 'vitest';
 
 import { unique } from '@array/unique';
 
-describe('uniq', () => {
+describe('unique', () => {
     test('should return an array with only unique values', () => {
         const input = [1, 2, 3, 3, 4, 5, 5, 6];
         const expectedOutput = [1, 2, 3, 4, 5, 6];
@@ -14,5 +14,16 @@ describe('uniq', () => {
         ];
         const expectedOutput = [{ a: 1 }, { a: 2 }, [1, 2], [1, 2, 3]];
         expect(unique(input)).toEqual(expectedOutput);
+    });
+
+    test('should hande compareFN', () => {
+        expect(unique([2, 1, 2])).toEqual([2, 1]);
+
+        const users = [
+            { id: 1, name: 'a' },
+            { id: 1, name: 'c' }
+        ];
+
+        expect(unique(users, (a, b) => a.id === b.id)).toEqual([{ id: 1, name: 'a' }]);
     });
 });
