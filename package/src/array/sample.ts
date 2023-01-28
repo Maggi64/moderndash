8d88acbfa709e37cf87a05bae00eb597d0ad8e61
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-28 16:42:27 +0100

    Sample empty array behavior

--- package/src/array/sample.ts
+++ package/src/array/sample.ts
@@ -1,8 +1,9 @@
 /**
  * Gets a random element an array. A single element is returned by default.  
- * Specify the `size` parameter to get an array of multiple random elements.
+ * Specify the `multi` parameter to get an array of multiple random elements.
  *
- * If the array is empty, `undefined` is returned.
+ * If the array is empty, `undefined` is returned.  
+ * If `multi` is defined it returns an empty array.  
  * @example
  * sample([1, 2, 3, 4])
  * // => 2
@@ -15,16 +16,18 @@
  */
 
 export function sample<TArr>(array: TArr[]): TArr | undefined;
-export function sample<TArr>(array: TArr[], size: number): TArr[];
-export function sample<TArr>(array: TArr[], size?: number): TArr | undefined | TArr[] {
-    if (array.length === 0)
-        return undefined;
-    if (size === undefined)
+export function sample<TArr>(array: TArr[], multi: number): TArr[];
+export function sample<TArr>(array: TArr[], multi?: number): TArr | undefined | TArr[] {
+    if (multi === undefined) {
+        if (array.length === 0) return undefined;
         return getSingleSample(array);
+    }
+
+    if (multi && array.length === 0) return [];
 
     // Multiple samples
-    const result = new Array<TArr>(size);
-    for (let i = 0; i < size; i++) {
+    const result = new Array<TArr>(multi);
+    for (let i = 0; i < multi; i++) {
         result[i] = getSingleSample(array);
     }
     return result;
--- package/test/array/sample.test.ts
+++ package/test/array/sample.test.ts
@@ -15,12 +15,12 @@ describe('sample', () => {
         expect(result).toBeUndefined();
     });
 
-    test('should return an empty array if the input array is empty', () => {
+    test('return an empty array if the input array is empty', () => {
         const result = sample([], 3);
-        expect(result).toBeUndefined();
+        expect(result).toEqual([]);
     });
 
-    test('should return a random subset of the input array', () => {
+    test('return a random subset of the input array', () => {
         const array = [1, 2, 3, 4, 5];
         const result = sample([1, 2, 3, 4, 5], 3);
         expect(expect.arrayContaining(result)).toEqual(array);
