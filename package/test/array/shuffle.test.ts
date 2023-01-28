9921ddcc8d3becc034972f0d971bd3252176488d
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-28 11:58:35 +0100

    More stable shuffle test

--- package/test/array/shuffle.test.ts
+++ package/test/array/shuffle.test.ts
@@ -3,15 +3,16 @@ import { test, describe, expect } from 'vitest';
 import { shuffle } from '@array/shuffle';
 
 describe('shuffle', () => {
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
     test('should shuffle the elements of the array', () => {
-        const array = [1, 2, 3, 4, 5];
+        const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
         const shuffledArray = shuffle(array);
         expect(shuffledArray).not.toEqual(array);
     });
 
     test('should not modify the original array', () => {
-        const array = [1, 2, 3, 4, 5];
         shuffle(array);
-        expect(array).toEqual([1, 2, 3, 4, 5]);
+        expect(array).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
     });
 });
