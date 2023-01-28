715968528970e750640df9f4938095208dbb2fa6
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-28 11:56:29 +0100

    feat: consolidate intersectionFNs

--- package/src/array/intersection.ts
+++ package/src/array/intersection.ts
@@ -1,20 +1,47 @@
 import type { MinimumTwoArrays } from '@helpers/types';
 
-import { intersectionWith } from '@array/intersectionWith';
 import { isEqual } from '@validate/isEqual';
 
 /**
  * Creates an array of unique values that are included in all given arrays.
  * The order and references of result values are determined by the first array.
  *
- * @category Array
- * @param arrays - The arrays to inspect.
- * @returns Returns the new array of intersecting values.
+ * An compare function is optinal to specify how the elements of the arrays are compared.
+ * Default compare function is {@link isEqual}.
+ *
  * @example
  * intersection([2, 1], [2, 3])
  * // => [2]
+ *
+ * // ---- Custom compare function ----
+ * intersection((a, b) => Math.floor(a) === Math.floor(b), [1.2, 1.1], [1.3, 2.4])
+ * // => [1.2]
+ *
+ * // ---- Only compare by id ----
+ * const arr1 = [{ id: 1, name: 'Yeet' }, { id: 3, name: 'John' }];
+ * const arr2 = [{ id: 3, 'Carl' }, { id: 4, name: 'Max' }];
+ *
+ * intersection((a, b) => a.id === b.id, arr1, arr2)
+ * // => [{ id: 3, name: 'John' }]
+ * @category Array
+ * @param arrays - The arrays to inspect.
+ * @returns Returns the new array of intersecting values.
  */
 
-export function intersection<TInput>(...arrays: MinimumTwoArrays<TInput>): TInput[] {
-    return intersectionWith(isEqual, ...arrays);
+export function intersection<TArr>(...arrays: MinimumTwoArrays<TArr>): TArr[];
+export function intersection<TArr>(arrayOrCompFn: (a: TArr, b: TArr) => boolean, ...arrays: MinimumTwoArrays<TArr>): TArr[];
+export function intersection<TArr>(arrayOrCompFn: TArr[] | ((a: TArr, b: TArr) => boolean), ...arrays: MinimumTwoArrays<TArr>): TArr[] {
+    const withCompareFn = typeof arrayOrCompFn === 'function';
+    const compareFN = withCompareFn ? arrayOrCompFn as (a: TArr, b: TArr) => boolean : isEqual;
+
+    const [firstArray, ...restArrays] = withCompareFn ? arrays : [arrayOrCompFn, ...arrays];
+    const intersection: TArr[] = [];
+
+    firstArray.forEach(element => {
+        if (restArrays.every(array => array.some(item => compareFN(item, element)))) {
+            intersection.push(element);
+        }
+    });
+
+    return intersection;
 }
--- package/test/array/intersection.test.ts
+++ package/test/array/intersection.test.ts
@@ -1,36 +1,29 @@
-import { expect, it, describe } from 'vitest';
+import { expect, test, describe } from 'vitest';
 
 import { intersection } from '@array/intersection';
-import { intersectionBy } from '@array/intersectionBy';
-import { intersectionWith } from '@array/intersectionWith';
 
 describe('intersection', () => {
-    it('returns the correct intersections', () => {
+    test('returns the correct intersections', () => {
         const arr1 = [1, 2, 3, 4, 5];
         const arr2 = [3, 4, 5, 6, 7];
 
         const result = intersection(arr1, arr2);
         expect(result).toEqual([3, 4, 5]);
     });
-});
 
-describe('intersectionBy', () => {
-    it('returns the correct intersections', () => {
+    test('returns the correct intersections select special elements', () => {
         const arr1 = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
         const arr2 = [{ id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }, { id: 7 }];
 
-        const result = intersectionBy(value => value.id, arr1, arr2);
+        const result = intersection((a, b) => a.id === b.id, arr1, arr2);
         expect(result).toEqual([{ id: 3 }, { id: 4 }, { id: 5 }]);
     });
-});
 
-
-describe('intersectionWith', () => {
-    it('returns the correct intersections', () => {
+    test('returns the correct intersections with comporator', () => {
         const arr1 = [1, 2, 3, 4, 5];
         const arr2 = [3, 4, 5, 6, 7];
 
-        const result = intersectionWith((a, b) => a % 2 === 0 && a === b, arr1, arr2);
+        const result = intersection((a, b) => a % 2 === 0 && a === b, arr1, arr2);
         expect(result).toEqual([4]);
     });
 });
