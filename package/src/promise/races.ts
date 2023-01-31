e6a17f14d18fca5552cb0357f2e940dc44c3f5c2
Maximilian Dewald <maximilian@dewaldnet.de>
2023-01-31 16:33:59 +0100

    fix races doc

--- package/src/promise/races.ts
+++ package/src/promise/races.ts
@@ -5,7 +5,7 @@
  * @example
  * const promise1 = Promise.resolve(1);
  * const promise2 = Promise.resolve(2);
- * const promise2 = Promise.resolve(3);
+ * const promise3 = Promise.resolve(3);
  * 
  * const firstThree = await races(3, promise1, promise2, promise3);
  * // => [1, 2, 3]
