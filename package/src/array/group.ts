d288395ff610cae4c6a4324826cef68b5ba74c05
Maximilian Dewald <maximilian@dewaldnet.de>
2023-02-02 10:24:43 +0100

    fix: allow boolean for group, simplifiy docs

--- package/src/array/group.ts
+++ package/src/array/group.ts
@@ -1,27 +1,26 @@
 import type { ObjectKey } from '@type/ObjectKey';
 
 /**
- * Creates an object composed of keys generated from the results of running
- * each element of `collection` thru `iteratee`. The order of grouped values
- * is determined by the order they occur in `collection`. The corresponding
- * value of each key is an array of elements responsible for generating the
- * key.
+ * Creates an object with grouped items in the array.
+ * The critiria provides the key to group by.
  *
  * @example
  * group([6.1, 4.2, 6.3], Math.floor)
- * // => { '4': [4.2], '6': [6.1, 6.3] }
+ * // => { 4: [4.2], 6: [6.1, 6.3] }
  *
  * group([6.1, 4.2, 6.3], value => value > 5)
  * // => { 'false': [4.2], 'true': [6.1, 6.3] }
  * @param collection - The array or object to iterate over.
- * @param iteratee - The iteratee to transform keys.
- * @returns Returns the composed aggregate object.
+ * @param criteria - The criteria to group by.
+ * @returns An object with grouped items.
  */
 
-export function group<T, U extends ObjectKey>(array: T[], iteratee: (value: T) => U): Record<U, T[]> {
-    const result = {} as Record<U, T[]>;
+export function group<TArr, TKey extends ObjectKey>(array: TArr[], criteria: (value: TArr) => TKey | boolean): Record<TKey, TArr[]> {
+    const result = {} as Record<TKey, TArr[]>;
     for (const value of array) {
-        const key = iteratee(value);
+        let key = criteria(value);
+        if (typeof key === 'boolean')
+            key = key.toString() as TKey;
         // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
         result[key] = result[key] ?? [];
         result[key].push(value);
