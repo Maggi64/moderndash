5daad0ac2c57a98d1a5b0e517c5cca7b646ca056
Maximilian Dewald <maximilian@dewaldnet.de>
2023-02-12 14:57:41 +0100

    minify website build

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		minify: true,
+		sourcemap: true,
+		target: 'esnext'
+	}
 };
 
 export default config;
