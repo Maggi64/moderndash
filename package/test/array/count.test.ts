77b728c73d1160ac808dfb4adedeba662727de7a
Maximilian Dewald <maximilian@dewaldnet.de>
2023-02-25 20:58:50 +0100

    count | improve performance

--- package/test/array/count.test.ts
+++ package/test/array/count.test.ts
@@ -5,7 +5,7 @@ import { count } from '@array/count';
 describe('countBy function', () => {
     test('countBy', () => {
         const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        const even = (value: number) => (value % 2 === 0);
+        const even = (value: number) => (value % 2 === 0).toString();
         const result = count(array, even);
         expect(result).toEqual({ 'true': 5, 'false': 5 });
     });
