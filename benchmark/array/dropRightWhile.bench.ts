a531f0227d18f6bb74a3db493792050087760074
Maximilian Dewald <maximilian@dewaldnet.de>
2023-02-27 15:54:26 +0100

    dropWhile benchmarks

---
+++ benchmark/array/dropRightWhile.bench.ts
@@ -0,0 +1,20 @@
+import { dropRightWhile as lodashVersion } from 'lodash-es';
+import { dropRightWhile } from 'moderndash';
+import { bench, describe } from 'vitest';
+
+import { randomNumberArray, randomStringArray } from '../testData.js';
+
+describe('dropRightWhile', () => {
+    const numArray = randomNumberArray(200);
+    const stringArray = randomStringArray(200);
+
+    bench('moderndash', () => {
+        dropRightWhile(numArray, (num) => num < 20);
+        dropRightWhile(stringArray, (str) => str.includes('a'));
+    });
+
+    bench('lodash', () => {
+        lodashVersion(numArray, (num) => num < 20);
+        lodashVersion(stringArray, (str) => str.includes('a'));
+    });
+});
---
+++ benchmark/array/dropWhile.bench.ts
@@ -0,0 +1,20 @@
+import { dropWhile as lodashVersion } from 'lodash-es';
+import { dropWhile } from 'moderndash';
+import { bench, describe } from 'vitest';
+
+import { randomNumberArray, randomStringArray } from '../testData.js';
+
+describe('dropWhile', () => {
+    const numArray = randomNumberArray(200);
+    const stringArray = randomStringArray(200);
+
+    bench('moderndash', () => {
+        dropWhile(numArray, (num) => num < 20);
+        dropWhile(stringArray, (str) => str.includes('a'));
+    });
+
+    bench('lodash', () => {
+        lodashVersion(numArray, (num) => num < 20);
+        lodashVersion(stringArray, (str) => str.includes('a'));
+    });
+});
