dcbec26679c018601289571c38e6bc7f819869cc
Maximilian Dewald <maximilian@dewaldnet.de>
2023-03-04 18:27:04 +0100

    flatKeys bench

---
+++ benchmark/object/flatKeys.bench.ts
@@ -0,0 +1,15 @@
+import { flatKeys } from 'moderndash';
+import { crush as radashVersion } from 'radash';
+import { bench, describe } from 'vitest';
+
+describe('set', () => {
+    const obj = { a: { b: 2 }, c: { d: 3 }, e: { f: [1, 2] } };
+
+    bench('moderndash', () => {
+        flatKeys(obj);
+    });
+
+    bench('radash', () => {
+        radashVersion(obj);
+    });
+});
