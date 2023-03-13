9171c05f21f0afeddb27a80103a90d5015d2a48d
Maximilian Dewald <maximilian@dewaldnet.de>
2023-03-13 10:41:18 +0100

    use image from github host

--- website/src/routes/+page.server.ts
+++ website/src/routes/+page.server.ts
@@ -2,13 +2,11 @@ import type { PageServerLoad } from './$types';
 
 import readme from '../../../README.md?raw';
 
-import introSvg from '$assets/introCodeV6.svg';
 import { markdownParser } from '$utils/markdown.js';
 
 // remove first markdown image from readme
 const trimmedReadme = readme.replace(/!\[.*]\(.*\)/, '');
-const parsedMarkdown = markdownParser(trimmedReadme)
-    .replace('https://raw.githubusercontent.com/Maggi64/moderndash/main/website/src/assets/introCodeV6.svg', introSvg);
+const parsedMarkdown = markdownParser(trimmedReadme);
 
 export const load: PageServerLoad = (() => {
     return {
