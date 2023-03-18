e2fede88694497e3cbd8eb5be7f17e979fc89bc0
Maximilian Dewald <maximilian@dewaldnet.de>
2023-03-18 15:37:54 +0100

    add titlecase bench

---
+++ benchmark/string/titleCase.bench.ts
@@ -0,0 +1,28 @@
+import { startCase as lodashVersion } from 'lodash-es';
+import { titleCase } from 'moderndash';
+import { title as radashVersion } from 'radash';
+import { bench, describe } from 'vitest';
+
+import { randomStringArray } from '../testData.js';
+
+describe('titleCase', () => {
+    const stringArray = randomStringArray(200);
+
+    bench('moderndash', () => {
+        for (const str of stringArray) {
+            titleCase(str);
+        }
+    });
+
+    bench('lodash', () => {
+        for (const str of stringArray) {
+            lodashVersion(str);
+        }
+    });
+
+    bench('radash', () => {
+        for (const str of stringArray) {
+            radashVersion(str);
+        }
+    });
+});
