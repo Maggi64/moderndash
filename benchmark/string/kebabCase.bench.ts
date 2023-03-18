fe5b43ae84962aa346332d2e32495fbbd0b0eeb0
Maximilian Dewald <maximilian@dewaldnet.de>
2023-03-18 15:09:17 +0100

    add kebabCase benchmark

---
+++ benchmark/string/kebabCase.bench.ts
@@ -0,0 +1,28 @@
+import { kebabCase as lodashVersion } from 'lodash-es';
+import { kebabCase } from 'moderndash';
+import { dash as radashVersion } from 'radash';
+import { bench, describe } from 'vitest';
+
+import { randomStringArray } from '../testData.js';
+
+describe('kebabCase', () => {
+    const stringArray = randomStringArray(200);
+
+    bench('moderndash', () => {
+        for (const str of stringArray) {
+            kebabCase(str);
+        }
+    });
+
+    bench('lodash', () => {
+        for (const str of stringArray) {
+            lodashVersion(str);
+        }
+    });
+
+    bench('radash', () => {
+        for (const str of stringArray) {
+            radashVersion(str);
+        }
+    });
+});
