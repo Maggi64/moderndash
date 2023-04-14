a440fbc8870a4be53b0057c8e23822eb93c61771
Maximilian Dewald <maximilian@dewaldnet.de>
2023-04-14 13:27:35 +0200

    improve difference bench

--- benchmark/array/difference.bench.ts
+++ benchmark/array/difference.bench.ts
@@ -1,67 +1,82 @@
+/* eslint-disable unicorn/no-for-loop */
 import { difference as lodashVersion, differenceWith as lodashVersionWith } from "lodash-es";
-import { difference } from "moderndash";
+import { difference, difference2 } from "moderndash";
 import { diff as radashVersion } from "radash";
 import { difference as remedaVersion, differenceWith as remedaVersionWith } from "remeda";
 import { bench, describe } from "vitest";
 
 import { randomNumberArray, randomStringArray } from "../testData.js";
 
+const size = 100;
+
+const arrays1 = Array.from({ length: size }, () => randomNumberArray(50, 0, 50));
+const arrays2 = Array.from({ length: size }, () => randomNumberArray(50, 0, 50));
+const arrays3 = Array.from({ length: size }, () => randomNumberArray(50, 0, 50));
+const arrays4 = Array.from({ length: size }, () => randomNumberArray(50, 0, 50));
+
+const strArray1 = Array.from({ length: size }, () => randomStringArray(50));
+const strArray2 = Array.from({ length: size }, () => randomStringArray(50));
+
 describe("difference", () => {
-    const array1 = randomNumberArray(50, 0, 50);
-    const array2 = randomNumberArray(50, 0, 50);
-
-    const array3 = randomStringArray(50);
-    const array4 = randomStringArray(50);
-
     bench("moderndash", () => {
-        difference(array1, array2);
-        difference(array3, array4);
+        for (let i = 0; i < arrays1.length; i++) {
+            difference(arrays1[i], arrays2[i]);
+            difference(strArray1[i], strArray2[i]);
+        }
     });
 
     bench("lodash", () => {
-        lodashVersion(array1, array2);
-        lodashVersion(array3, array4);
+        for (let i = 0; i < arrays1.length; i++) {
+            lodashVersion(arrays1[i], arrays2[i]);
+            lodashVersion(strArray1[i], strArray2[i]);
+        }
     });
     
     bench("radash", () => {
-        radashVersion(array1, array2);
-        radashVersion(array3, array4);
+        for (let i = 0; i < arrays1.length; i++) {
+            radashVersion(arrays1[i], arrays2[i]);
+            radashVersion(strArray1[i], strArray2[i]);
+        }
     });
 
     bench("remeda", () => {
-        remedaVersion(array1, array2);
-        remedaVersion(array3, array4);
+        for (let i = 0; i < arrays1.length; i++) {
+            remedaVersion(arrays1[i], arrays2[i]);
+            remedaVersion(strArray1[i], strArray2[i]);
+        }
     });
 });
 
 describe("difference 4 arrays", () => {
-    const array1 = randomNumberArray(50, 0, 50);
-    const array2 = randomNumberArray(50, 0, 50);
-    const array3 = randomNumberArray(50, 0, 50);
-    const array4 = randomNumberArray(50, 0, 50);
-
     bench("moderndash", () => {
-        difference(array1, array2, array3, array4);
+        for (let i = 0; i < arrays1.length; i++) {
+            difference(arrays1[i], arrays2[i], arrays3[i], arrays4[i]);
+        }
     });
 
     bench("lodash", () => {
-        lodashVersion(array1, array2, array3, array4);
+        for (let i = 0; i < arrays1.length; i++) {
+            lodashVersion(arrays1[i], arrays2[i], arrays3[i], arrays4[i]);
+        }
     });
 });
 
 describe("difference compareFn", () => {
-    const array1 = randomNumberArray(50, 0, 50);
-    const array2 = randomNumberArray(50, 0, 50);
-    
     bench("moderndash", () => {
-        difference(array1, array2, (a, b) => a - 1 === b);
+        for (let i = 0; i < arrays1.length; i++) {
+            difference(arrays1[i], arrays2[i], (a, b) => a - 1 === b);
+        }
     });
 
     bench("lodash", () => {
-        lodashVersionWith(array1, array2, (a, b) => a - 1 === b);
+        for (let i = 0; i < arrays1.length; i++) {
+            lodashVersionWith(arrays1[i], arrays2[i], (a, b) => a - 1 === b);
+        }
     });
 
     bench("remeda", () => {
-        remedaVersionWith(array1, array2, (a, b) => a - 1 === b);
+        for (let i = 0; i < arrays1.length; i++) {
+            remedaVersionWith(arrays1[i], arrays2[i], (a, b) => a - 1 === b);
+        }
     });
 });
