304d42eeec3a7b8a30eaff86698becc3c6c355e7
Maximilian Dewald <maximilian.dewald@envelio.de>
2024-05-11 20:15:11 +0200

    deploy vite-tsconfig-paths

--- package/vitest.config.ts
+++ package/vitest.config.ts
@@ -1,32 +1,15 @@
 /// <reference types="vitest" />
 // Configure Vitest (https://vitest.dev/config/)
 
-import { fileURLToPath, URL } from "node:url";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-const getPath = (path: string) => fileURLToPath(new URL(path, import.meta.url));
-
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
         globals: true,
         coverage: {
             exclude: ["**/index.ts", "src/type", "test/**", "src/helpers/ArrayTypeUtils.ts"]
-        }
-    },
-    resolve: {
-        alias: {
-            "@array": getPath("src/array"),
-            "@object": getPath("src/object"),
-            "@function": getPath("src/function"),
-            "@string": getPath("src/string"),
-            "@crypto": getPath("src/crypto"),
-            "@number": getPath("src/number"),
-            "@helpers": getPath("src/helpers"),
-            "@promise": getPath("src/promise"),
-            "@validate": getPath("src/validate"),
-            "@type": getPath("src/type"),
-            "@decorator": getPath("src/decorator")
         }
     }
 });
